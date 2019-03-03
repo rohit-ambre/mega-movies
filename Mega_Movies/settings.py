08fedb8135c8afe701392c0cc965ffdde9ecf237
Rohit Ambre <rohit003.ambre@gmail.com>
2019-03-03 17:23:23 +0530

    added user app and added apps in settings

--- Mega_Movies/settings.py
+++ Mega_Movies/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'BookMyMovie.apps.BookmymovieConfig',
+    'User.apps.UserConfig',
 ]
 
 MIDDLEWARE = [
