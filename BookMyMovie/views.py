3b90574143d30e7b687c3afc1297ca461ef6f413
Rohit Ambre <rohit003.ambre@gmail.com>
2019-03-18 22:53:51 +0530

    added 404 and 500 error pages

--- BookMyMovie/views.py
+++ BookMyMovie/views.py
@@ -8,3 +8,11 @@ def landing(request):
 
 def index(request):
     return render(request,'BookMyMovie/BookMovie.html')
+
+def handler404(request, exception):
+    data = {"name": "You entered something wrong here"}
+    return render(request,'BookMyMovie/404.html', data)
+
+def handler500(request, exception):
+    data = {"name": "There is some error"}
+    return render(request,'BookMyMovie/500.html', data)
