ad935d04b8f8a48027e4dd38fd761539875ef544
Rohit Ambre <rohit003.ambre@gmail.com>
2019-03-23 23:56:56 +0530

    added individual movie info on display page

--- Movies/views.py
+++ Movies/views.py
@@ -3,14 +3,12 @@ import requests
 # Create your views here.
 
 def landing(request):
-    url = 'http://www.omdbapi.com/?apikey=******&r=json&s={}'
+    url = 'http://www.omdbapi.com/?apikey=1fc933fe&r=json&s={}'
     
     if request.method == 'POST':
         term = request.POST.get("search", "")
         res = requests.get(url.format(term))
         data = {'movies':res.json()}
-        # print(res.text)
-        # return render(request,'Movies/landing.html',data)
     else:
         term = 'Thor'
         res = requests.get(url.format(term))
@@ -21,7 +19,7 @@ def testredirect(request):
     return redirect('/Movies')
 
 def display(request, imdb):
-    url = 'http://www.omdbapi.com/?apikey=******&r=json&i={}'
+    url = 'http://www.omdbapi.com/?apikey=1fc933fe&r=json&i={}'
     
     if imdb:
         res = requests.get(url.format(imdb))
