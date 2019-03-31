56773f27c5e2c6e1271bb8f1fe905581efa002d2
Rohit Ambre <rohit003.ambre@gmail.com>
2019-03-31 15:15:21 +0530

    added carousel on bookmymovie page and container from base

--- BookMyMovie/views.py
+++ BookMyMovie/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import requests
+from BookMyMovie.models import Movie
 # Create your views here.
 
 
 def index(request):
-    return render(request,'BookMyMovie/BookMovie.html')
+    movies = Movie.objects.all()
+
+    return render(request,'BookMyMovie/BookMovie.html',{'movies':movies})
 
 def handler404(request, exception):
     data = {"name": "You entered something wrong here"}
