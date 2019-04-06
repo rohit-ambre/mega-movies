287ed03be0791f01758c6af5f49f59ca36035e83
Rohit Ambre <rohit003.ambre@gmail.com>
2019-04-07 01:16:13 +0530

    added calender view- incomplete

--- BookMyMovie/urls.py
+++ BookMyMovie/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
     path('',views.index, name='index'),
-    path('<str:m>', views.movie, name='movie')
+    path('<int:m>', views.movie, name='movie'),
+    # path(r'^[0-9]/calender/$', views.calender, name='calender')
+
 ]
--- BookMyMovie/views.py
+++ BookMyMovie/views.py
@@ -22,3 +22,7 @@ def movie(request,m):
     movie = Movie.objects.get(id=m)
     data = {'movie':movie}
     return render(request,'BookMyMovie/BMmovie.html',data)
+
+def calender(request):
+
+    return render(request,'BookMyMovie/calender.html')
